48aedd85c2322cef7554160dc9e5750d4c3fbc55
Yuki Takenaka <yuki.takenaka@andco.group>
2020-01-10 18:33:50 +0900

    add material-ui

--- src/components/pages/Index.tsx
+++ src/components/pages/Index.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'stores';
 import userSlice, { setRandomName } from 'stores/user';
+import { Button, Typography } from '@material-ui/core';
 
 export default () => {
   const dispatch = useDispatch();
@@ -24,21 +25,29 @@ export default () => {
 
   return (
     <App>
-      <h1>Index</h1>
+      <Typography variant="h1" component="h1">
+        Index
+      </Typography>
 
       <h2>{name}</h2>
 
       <input value={name} type="text" onChange={onTextChange} />
 
+      <StyleButton color="primary">hello</StyleButton>
       <button onClick={onClearClicked}>clear</button>
       <button onClick={onRandomClicked}>random</button>
     </App>
   );
 };
 
-const App = styled.div`
-  h2 {
-    color: gray;
-    height: 2rem;
-  }
+const App = styled.div``;
+
+const StyleButton = styled(Button)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 3px;
+  border: 0;
+  color: ${props => props.theme.palette.primary.main};
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
 `;
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
 import Index from 'components/pages/Index';
 import Count from 'components/pages/Count';
 import * as serviceWorker from 'serviceWorker';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import store from 'stores';
 import { Provider } from 'react-redux';
+import { AppBar, TextField, Toolbar, Typography } from '@material-ui/core';
+import Styles from 'components/layouts/Styles';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <nav>
-        <Link style={{ marginRight: '8px' }} to="/">
-          index
-        </Link>
-        <Link to="/count">count</Link>
-      </nav>
-      <Switch>
-        <Route path="/" exact children={<Index />} />
-        <Route path="/count" children={<Count />} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <Styles>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppBar position="absolute">
+          <Toolbar>
+            <Typography component="h1" variant="h6" color="inherit" noWrap>
+              Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <TextField />
+        <Switch>
+          <Route path="/" exact children={<Index />} />
+          <Route path="/count" children={<Count />} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </Styles>,
   document.getElementById('root')
 );
 
