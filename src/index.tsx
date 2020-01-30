603c44ece9a8a4f6349cca93d5317f7e93ed9585
Yuki Takenaka <yuki.takenaka@andco.group>
2020-01-30 17:51:34 +0900

    不要なコンポーネントの削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from 'components/pages/Index';
-import Count from 'components/pages/Count';
 import * as serviceWorker from 'serviceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import store from 'stores';
 import { Provider } from 'react-redux';
-import { AppBar, TextField, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import Styles from 'components/layouts/Styles';
 
 ReactDOM.render(
   <Styles>
     <Provider store={store}>
+      <AppBar position="absolute">
+        <Toolbar>
+          <Typography component="h1" variant="h6" color="inherit" noWrap>
+            Sample App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
       <BrowserRouter>
-        <AppBar position="absolute">
-          <Toolbar>
-            <Typography component="h1" variant="h6" color="inherit" noWrap>
-              Dashboard
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <TextField />
         <Switch>
           <Route path="/" exact children={<Index />} />
-          <Route path="/count" children={<Count />} />
         </Switch>
       </BrowserRouter>
     </Provider>
